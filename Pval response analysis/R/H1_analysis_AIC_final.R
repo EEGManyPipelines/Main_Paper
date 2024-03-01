ead152678c39a8b6ce23f04db728734a3cf7a8ed
Elena Cesnaite <e.cesnaite@gmail.com>
2024-03-01 15:40:02 +0100

    updated the AIC code
    solved the multicollinearity problem

--- Pval response analysis/R/H1_analysis_AIC_final.R
+++ Pval response analysis/R/H1_analysis_AIC_final.R
@@ -15,9 +15,9 @@ library(ggplot2)
 h1_data <- read.csv("C:/Users/ecesnait/Desktop/EEGManyPipelines/Data/Big Analysis/all_var_AQ_h1.csv")
 h1_data <- as.data.frame(h1_data)
 
-# --------------------
+## --------------------
 # Correct values
-# --------------------
+## --------------------
 
 # Software
 h1_data$software[h1_data$software== 'eeglab_erplab'] <- 'eeglab'
@@ -35,6 +35,7 @@ h1_data$ans_exclusion_criteria_seg[h1_data$ans_exclusion_criteria_seg == ''] <- 
 #Time window start, end & length
 #exchange missing values to the mean of the column
 h1_data$ans_time_w_start_h1[is.na(h1_data$ans_time_w_start_h1)] <- mean((h1_data$ans_time_w_start_h1), na.rm = T) 
+
 h1_data$ans_time_w_end_h1[is.na(h1_data$ans_time_w_end_h1)] <- mean((h1_data$ans_time_w_end_h1), na.rm = T) 
 h1_data$time_w_length_h1[is.na(h1_data$time_w_length_h1)] <- mean((h1_data$time_w_length_h1), na.rm = T) 
 # ICA algorythm
@@ -43,12 +44,79 @@ h1_data$ans_ica_algo[h1_data$ans_ica_algo == ''] <- 'unknown'
 #Baseline start and stop
 h1_data$ans_baseline_start[h1_data$ans_baseline_start == '-200 ms'] <- '-200'
 h1_data$ans_baseline_start[h1_data$ans_baseline_start == '-200ms'] <- '-200'
+
 h1_data$ans_baseline_start <- as.numeric(h1_data$ans_baseline_start)
 h1_data$ans_baseline_start[is.na(h1_data$ans_baseline_start)] <- mean((h1_data$ans_baseline_start), na.rm = T) 
-h1_data$ans_baseline_stop[is.na(h1_data$ans_baseline_stop)] <- mean((h1_data$ans_baseline_stop), na.rm = T) 
+h1_data$ans_baseline_stop[is.na(h1_data$ans_baseline_stop)] <- mean((h1_data$ans_baseline_stop), na.rm = T)
+
+# Change baseline window length 0 to the mean of the column because 0 is dragging the effect
+h1_data$bs_window_length[h1_data$bs_window_length == 0] <- mean(h1_data$bs_window_length[h1_data$bs_window_length != 0], na.rm = T) 
+
 
 #remove binary response
 data <- h1_data[,c(-1,-27)]
+
+## --------------------
+# outliers removal
+## --------------------
+# 
+hf_out <- boxplot(data$hf_cutoff)$out
+data$hf_cutoff[which(data$hf_cutoff == hf_out)] <- mean((data$hf_cutoff), na.rm = T) # removed one team that had 3 for a  HP filter
+
+## --------------------
+# from p-val to z-val
+## --------------------
+# 
+hist(qnorm(data$pval))
+data$pval <- qnorm(data$pval)#qnorm(0.975,mean=0,sd=1)
+
+## --------------------
+# look if there are any NaN values
+## --------------------
+# 
+empty_row <- which(is.na(data),arr.ind=TRUE) # 
+indx_row <- empty_row[,1]
+# Caution: remove them from the dataset
+data <- data[-indx_row,]
+
+
+## --------------------
+# scale continuous data
+## --------------------
+# 
+
+continuous <- c(3, 7, 8, 10, 16:18, 23:25)
+
+#normalizing data
+for (i in 1:length(continuous)) {
+  contin_data_col <- data[,continuous[i]]
+  data[,continuous[i]] <- scale(contin_data_col)
+  #hist(scale(contin_data_col))
+}
+
+
+## --------------------
+# combine multiple comparison and statistical test columns (avoid the cluster problem)
+## --------------------
+# 
+table(data$mc_method_h1)
+# add correction method next to the stat model
+for (i in 1:nrow(data)) {
+  if (data$mc_method_h1[i] == 'bonferroni') {
+    data$stat_method_h1[i] <- paste0(data$stat_method_h1[i],'_bonf')
+  } else if (data$mc_method_h1[i] == 'bhfdr') {
+    data$stat_method_h1[i] <- paste0(data$stat_method_h1[i],'_bhfdr')
+  } else if (data$mc_method_h1[i] =='holm-bonferroni'){
+    data$stat_method_h1[i] <- paste0(data$stat_method_h1[i],'_hbonf')
+  }else if (data$mc_method_h1[i] =='rft'){
+    data$stat_method_h1[i] <- paste0(data$stat_method_h1[i],'_rft')
+  }
+}
+
+# Now i can remove the multiple comparisons questions from the table
+data <- data[,c(-12)]
+
+## ------------------------------------------------------------------------------------
 
 #Converting the categorical variables in the data to factors
 data$software <- as.factor(data$software)
@@ -67,62 +135,108 @@ data$ans_ica_algo <- as.factor(data$ans_ica_algo)
 data$ans_bad_comp_sel_visual <- as.factor(data$ans_bad_comp_sel_visual)
 data$ans_bad_comp_sel_plugin <- as.factor(data$ans_bad_comp_sel_plugin)
 
-# outliers removal
-hf_out <- boxplot(data$hf_cutoff)$out
-data$hf_cutoff[which(data$hf_cutoff == hf_out)] <- mean((data$hf_cutoff), na.rm = T) # removed one team that had 3 for a  HP filter
-
-# from p-val to z-val
-hist(qnorm(data$pval))
-data$pval <- qnorm(data$pval)#qnorm(0.975,mean=0,sd=1)
-
-# look if there are any NaN values
-which(is.na(data),arr.ind=TRUE) # 
-
-# Caution: remove them from the dataset
-data <- data[-160,]
-
-continuous <- c(3, 7, 8, 10, 16:18, 23:25)
-
-#normalizing data
-for (i in 1:length(continuous)) {
-  contin_data_col <- data[,continuous[i]]
-  data[,continuous[i]] <- scale(contin_data_col)
+ #---------------------------------------------------------------------------------------------------------------------------
+#remove dummy variables that have too few observations
+#install.packages('fastDummies')
+library(fastDummies)
+dummy_data <- dummy_cols(data, remove_selected_columns = TRUE)
+#remove columns with few observations
+n=1
+columns_exclude <- c()
+for (i in 12:length(dummy_data)) {
+  if (sum(dummy_data[,i]) < 6) {
+    columns_exclude[n] = i
+    n = n+1
+  }
 }
-## ------------------------------------------------------------------------------------
+names <- colnames(dummy_data)
+names[columns_exclude]
+dummy_data <- dummy_data[,-columns_exclude]
 
-# Remove/keep one of: time window start, stop, length; Baseline start, stop, length
-data_tw_start_stop <- data[,c(-18,-25)]
-data_tw_full <- data[,c(-16,-17,-23,-24)]
+#---------------------------------------------------------------------------------------------------------------------------
+
+# exclude variables that have high collinearity >0.6
+collinearity_indx <- match(c("topo_region_h1",
+                             "ans_time_w_end_h1", "ans_time_w_start_h1", "ans_baseline_start", "ans_hf_direction"), colnames(data)) # 37
+data_reduced <- data[,-collinearity_indx] 
+#inclusing categorical data
+#install.packages('ggcorrplot')
+library(ggcorrplot)
+library(tidyr)
+#inclusing categorical data
+#install.packages('ggcorrplot')
+library(ggcorrplot)
+library(tidyr)
+#find the ones above 0.8
+all_cor <- model.matrix(~0+., data=data_reduced) %>% 
+  cor(use="pairwise.complete.obs")
+which(abs(all_cor)>0.6 & abs(all_cor)!=1, arr.ind=TRUE) # 
+cor(data_reduced$hf_cutoff, data_reduced$ds_fs)
+
+#---------------------------------------------------------------------------------------------------------------------------
 
 #Approach 1. Using the step function.
 #Checking different models using the step function. 
-fullmodel<- lm(pval~ . , data = data_tw_start_stop)
+fullmodel<- lm(pval~ . , data = data_reduced)
 summary(fullmodel)
-stepwise_model <- step(fullmodel, direction = "both", trace = FALSE, k = log(nrow(data_tw_start_stop)))
+
+# Stepwise model
+stepwise_model <- step(fullmodel, direction = "both", trace = T, k = log(nrow(data_reduced)))
 summary(stepwise_model)
+
 all_aic_values = data.frame(stepwise_model$anova$Step,stepwise_model$anova$AIC)
 
 #The factors that had the most effect on the model are: ans_mt_h1,bs_window_start
 #So I decided to use those along with their interaction effect
-interactionformula = pval~ ans_mt_h1 + ans_baseline_start + ans_mt_h1*ans_baseline_start
 
-interactionmodel <- lm(interactionformula, data=data_tw_start_stop)
+winmodel <- lm(pval~ bs_window_length+ans_mt_h1+bs_window_length*ans_mt_h1, data=data_reduced)
 
-summary(interactionmodel)
-summ(interactionmodel, confint = TRUE, digits = 4) # nicer vizualisation
+summary(winmodel)
+summ(winmodel, confint = TRUE, digits = 4) # nicer vizualisation
 
 #add fitted regression line to scatterplot
-fit <-  lm(pval ~ ans_baseline_start, data=data_tw_start_stop)
+fit <-  lm(pval ~ bs_window_length, data=data_reduced)
 #create scatterplot
-plot(pval ~ ans_baseline_start, data=data_tw_start_stop)
+plot(pval ~ bs_window_length, data=data_reduced)
 abline(fit)
+summ(fit,confint = TRUE, digits = 4)
 
 #Interaction
-ggplot(data=data_tw_start_stop, aes(x=ans_baseline_start, y=pval, group=ans_mt_h1))+
+ggplot(data=data_reduced, aes(x=bs_window_length, y=pval, group=ans_mt_h1))+
   geom_point(size=2, aes(color=ans_mt_h1))+
+  geom_smooth(method= "lm")+
   ylab("qnorm(pval)")+
   xlab("baseline start")+
   ggtitle("Interaction effect")
+
+# with original values
+h1_data$ans_mt_h1 <- as.factor(h1_data$ans_mt_h1)
+h1_data <- h1_data[!is.na(h1_data$ans_mt_h1),]
+
+table(h1_data$mc_method_h1)
+h1_data$mc_method_h1[!h1_data$mc_method_h1 == "permutation"] <- "not_prm"
+
+tiff("C:/Users/ecesnait/Desktop/EEGManyPipelines/Figures/h1_interaction_baseline_mc.png", units="in", width=7, height=4.5, res=300)
+
+ggplot(data=h1_data, aes(x=bs_window_length, y=qnorm(pval), group=ans_mt_h1 ))+
+  geom_point(size=4, aes(color=ans_mt_h1, shape = mc_method_h1), alpha = 0.5)+
+  geom_smooth(method= "lm")+
+  scale_shape_manual(values = c(16,17)) +
+  scale_color_manual(values = c("#00AFBB", "#E7B800"))+
+  theme_minimal()+
+  ylab("qnorm(pval)")+
+  xlab("baseline length(ms)")+
+  ggtitle("Interaction effect")+ theme(text = element_text(size=15))
+dev.off()
+
+# baseline window without the mt
+ggplot(data=h1_data, aes(x=bs_window_length, y=qnorm(pval) ))+
+  geom_point(size=2)+
+  geom_smooth(method= "lm")+
+  ylab("qnorm(pval)")+
+  xlab("baseline length(ms)")+
+  ggtitle("Interaction effect")
+
 
 ## Check assumptions ##
 
